4c8e75837718ff8725dd38f5f44719280259d59e
javochoa <javochoa@JAVOCHOA-LAP.mx.oracle.com>
2019-11-12 17:58:05 -0600

    loadQuestions works

--- vegefoods/js/quizz.js
+++ vegefoods/js/quizz.js
@@ -7,6 +7,19 @@ let index = 0;
 let min = 0;
 let sec = 0;
 
+let addCorrectQuestions = (quizz) => {
+    let correct = quizz.questions.map((item) => {
+        return item.answers.map((answer) => {
+            if(answer.correct == true){
+                return answer.answer;
+            }
+        });
+    });
+    quizz.correct = correct;
+}
+
+
+
 let loadQuizz = (cb) => {
     let xhr = new XMLHttpRequest();
     let endpoint = `http://localhost:3000/quizzes/${localStorage.pin}`
@@ -15,8 +28,8 @@ let loadQuizz = (cb) => {
     xhr.send();
     xhr.onload = () => {
         if(xhr.status == 200){
-            //console.log(xhr.response);
             quizz = JSON.parse(xhr.response);
+            addCorrectQuestions(quizz);
             cb(index);
         }
     }
@@ -30,14 +43,13 @@ let loadQuizzResults = () => {
     xhr.send();
     xhr.onload = () => {
         if(xhr.status == 200){
-            //console.log(xhr.response);
             quizzResults = JSON.parse(xhr.response);
+            quizzResults.answers = [];
         }
     }
 }
 
 let loadQuestions = (index) => {
-    //console.log(quizz);
     question.innerText = quizz.questions[index].question;
     let html = quizz.questions[index].answers.map((item) => {
         return `
@@ -49,13 +61,11 @@ let loadQuestions = (index) => {
             </div>
         </div>`
     }).join("");
-    //console.log(html);
     answers.innerHTML = html;
     console.log(quizz.questions[index]);
     console.log(quizz.questions[index].time);
     min = Math.floor(quizz.questions[index].time / 60);
-    sec = quizz.questions[index].time % 60;
-    //console.log(answers);
+    sec = quizz.questions[index].time % 60 | 0;
 }
 
 loadQuizz(loadQuestions);
@@ -64,11 +74,40 @@ loadQuizzResults();
 function makeTimer() {
     $("#minutes").html(min + "<span>Minutes</span>");
     $("#seconds").html(sec + "<span>Seconds</span>");
-    sec--;
     if(sec == 0){
         min--;
         sec = 60;
     }
+    sec--;
+    //Store answer null
+    //loadNextQuestion
 }
 
 setInterval(function() { makeTimer(); }, 1000);
+
+function storeAnswer(event) {
+    if(event.target.className != "text py-3 pb-4 px-3 text-center" && event.target.nodeName != "H3" ){
+        return; 
+    }
+
+    let newAnswer = {};
+    newAnswer.time = quizz.questions[index].time - ((min * 60) + sec);
+    newAnswer.question = quizz.questions[index].question;
+    newAnswer.answer = event.target.innerText;
+    
+    let found = quizz.correct[index].find(x => {
+        if(x == undefined) return;
+        return x.toUpperCase() == event.target.innerText
+    });
+
+    if(found == undefined){
+        newAnswer.correct = false;
+    }else {
+        newAnswer.correct = true;
+    }
+    quizzResults.answers[index] = newAnswer;
+    index++;
+    loadQuestions(index);
+}
+
+answers.addEventListener("click",storeAnswer);
