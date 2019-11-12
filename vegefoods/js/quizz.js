f58c684471610ad745d1d7c6921bf2672ff065f9
marianasrvg <sierravgmr@gmail.com>
2019-11-12 16:30:07 -0600

    fix timer

--- vegefoods/js/quizz.js
+++ vegefoods/js/quizz.js
@@ -4,6 +4,8 @@ let quizz;
 let question = document.querySelector("#question");
 let answers = document.querySelector("#answers");
 let index = 0;
+let min = 0;
+let sec = 0;
 
 let loadQuizz = (cb) => {
     let xhr = new XMLHttpRequest();
@@ -13,7 +15,7 @@ let loadQuizz = (cb) => {
     xhr.send();
     xhr.onload = () => {
         if(xhr.status == 200){
-            console.log(xhr.response);
+            //console.log(xhr.response);
             quizz = JSON.parse(xhr.response);
             cb(index);
         }
@@ -28,31 +30,45 @@ let loadQuizzResults = () => {
     xhr.send();
     xhr.onload = () => {
         if(xhr.status == 200){
-            console.log(xhr.response);
+            //console.log(xhr.response);
             quizzResults = JSON.parse(xhr.response);
         }
     }
 }
 
 let loadQuestions = (index) => {
-    console.log(quizz);
+    //console.log(quizz);
     question.innerText = quizz.questions[index].question;
     let html = quizz.questions[index].answers.map((item) => {
-        return `<div class="col-md-6 col-lg-4 ftco-animate">
-                    <div class="product">
-                        <div class="text py-3 pb-4 px-3 text-center">
-                            <h3>${item.answer}</h3>
-                        </div>
-                    </div>
+        return `
+        <div class="col-md-6 col-lg-4">
+            <div class="product">
+                <div class="text py-3 pb-4 px-3 text-center">
+                    <h3>${item.answer}</h3>
                 </div>
-                `
+            </div>
+        </div>`
     }).join("");
-    console.log(html);
+    //console.log(html);
     answers.innerHTML = html;
-    console.log(answers);
+    console.log(quizz.questions[index]);
+    console.log(quizz.questions[index].time);
+    min = Math.floor(quizz.questions[index].time / 60);
+    sec = quizz.questions[index].time % 60;
+    //console.log(answers);
 }
-
-
 
 loadQuizz(loadQuestions);
 loadQuizzResults();
+
+function makeTimer() {
+    $("#minutes").html(min + "<span>Minutes</span>");
+    $("#seconds").html(sec + "<span>Seconds</span>");
+    sec--;
+    if(sec == 0){
+        min--;
+        sec = 60;
+    }
+}
+
+setInterval(function() { makeTimer(); }, 1000);
