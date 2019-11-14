eda57b36cada5f7e1ca80e6be952f2c2b3192a50
marianasrvg <sierravgmr@gmail.com>
2019-11-14 08:03:31 -0600

    Delete quizz owned

--- vegefoods/js/quizzes_owned.js
+++ vegefoods/js/quizzes_owned.js
@@ -1,4 +1,5 @@
 let quizzesTable = document.querySelector("tbody");
+let closeBtn;
 let quizzes;
 
 function loadQuizzesInfo () {
@@ -36,12 +37,40 @@ let loadQuizzesHTML = () => {
         <td class="price">${quizz.id}</td>
 
         <td class="product-remove">
-            <a href="#"><span class="ion-ios-close"></span></a>
+            <a class="delete" href="#"><span class="ion-ios-close"></span></a>
             <a href="#"><span class="ion-ios-more"></span></a>
         </td>
     </tr>`;
     }).join("\n");
+    addDeleteClass();
+}
 
+let deleteQuizz = (event) => {
+    pin = event.currentTarget.parentElement.previousElementSibling.innerText;
+    let option = confirm(`¿Deseas eliminar el quizz con el pin ${pin}`);
+    if(option == true) {
+        let xhr = new XMLHttpRequest();
+        let endpoint = `http://localhost:3000/quizzes/${pin}`;
+        xhr.open('DELETE', endpoint);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send();
+        xhr.onload = () => {
+            if(xhr.status == 200){
+                console.log(xhr.response);
+                //quizzes = JSON.parse(xhr.response);
+                //loadQuizzesHTML();
+            }
+        }
+    } 
+    return;
+}
+
+let addDeleteClass = () => {
+    closeBtn = document.querySelectorAll(".delete");
+    //console.log(closeBtn);
+    closeBtn.forEach((element) => {
+        element.addEventListener("click", deleteQuizz);
+    });
 }
 
 let verifyUser = () => {
@@ -53,9 +82,10 @@ let verifyUser = () => {
 }
 
 let quizzDetails = (pin) => {
-    console.log(`Details ${pin}`);
+    //console.log(`Details ${pin}`);
     localStorage.quizzDetails = pin;
     window.location.href = "quizz-info.html";
 }
 
+//console.log(closeBtn);
 verifyUser();
