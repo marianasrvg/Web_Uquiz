e5465d27d531e6e11258f876b45ca08d060bf4d2
Javier8ap <Javier8apar@gmail.com>
2019-12-03 12:32:03 -0600

    switched middlewares from users.js to separate file middlewares.js

--- server/login.js
+++ server/login.js
@@ -32,12 +32,13 @@ router.post("/",(req,res) => {
     const validUser = users.find( e => e.email === req.body.email &&  e.password === req.body.password);
 
     if(!(validUser)){
-        res.status(401).send("Invalid credentials");
+        res.status(400).send("Invalid credentials");
     }else{
         
         let resBody = {};
+        let privatePassword = "securePassword";
         //When a valid user is found, sign a token and send it back
-        let token = jwt.sign({email:req.body.email, id:validUser.id},validUser.password,
+        let token = jwt.sign({email:req.body.email, id:validUser.id},privatePassword,
                         {expiresIn:60*60*6} )
 
         resBody.token = token;
@@ -45,10 +46,9 @@ router.post("/",(req,res) => {
         resBody.id = validUser.id;
         resBody.admin = validUser.admin;
         //fs.writeFileSync('users.json',JSON.stringify(users));
-        passwords.password = validUser.password;
+        passwords.password = privatePassword;
         fs.writeFileSync('passwords.json',JSON.stringify(passwords));
         res.status(200).send(resBody);
-   
     }
 });
 
--- server/users.js
+++ server/users.js
@@ -1,71 +1,13 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
 const fs = require('fs');
 const joi = require('joi');
-const jwt = require('jsonwebtoken');
+const middlewares = require('./middlewares');
 
 let users = JSON.parse(fs.readFileSync('users.json'));
-let passwords = JSON.parse(fs.readFileSync('passwords.json'));
-
-app.use(express.json());
-
-//---------------------------------------MIDDLEWARES----------------------------------------------------
-let adminValidator = (req,res,next) => {
-    let validUser = users.find(e => e.id == req.body.id);
-    if(validUser.admin == 1){
-        next(); 
-    }
-    else{
-        res.status(401).send("You must be an admin");
-    }    
-}
-
-let tokenValidator = (req,res,next) => {
-
-    const userToken = req.header('x-auth-user');
-    
-    if(!userToken){
-        res.status(401).send('session token not found');
-        return
-    }
 
 
-    let userPassword = passwords.password;
 
-    jwt.verify(userToken, userPassword,(err,decoded)=>{
-        if(err){
-            if(err.name == 'TokenExpiredError'){
-                res.status(403).send("token has expired");
-                return
-            }else{
-                res.status(401).send("invalid token");
-                return
-            }
-        }
-        req.body.id = decoded.id;
-        //let decoded2 = jwt.decode(userToken,{complete:true});
-        //req.body.id = decoded.id;
-    });
-
-    next();
-}
-
-let adminParamValidator = (req,res,next) =>{
-    let validUser = users.find(e => e.id == req.body.id);
-    let paramID = req.params.id;
-    console.log(validUser);
-    if(validUser.admin == 1){
-        next(); 
-    }
-    else{
-        if(paramID == req.body.id){
-            next();
-        }else{
-            res.status(401).send("You can only search your own user or must be admin");
-        }  
-    }
-}
 
 //---------------------------------------API-------------------------------------------------------------
 
@@ -105,20 +47,20 @@ router.post('/',(req, res)=>{
     }
 });
 
-router.get('/:id',tokenValidator,adminParamValidator, (req,res)=>{
+router.get('/:id',middlewares.tokenValidator,middlewares.permissionValidator, (req,res)=>{
     let paramID = req.params.id;
     let user = users.find(e => e.id == paramID);
     res.send(user).status(200);
     return
 });
 
-router.get("/",tokenValidator,adminValidator, (req, res) => {
+router.get("/",middlewares.tokenValidator,middlewares.adminValidator, (req, res) => {
     //Mostrar todos los usuarios
     res.send(users);
     return
 });
 
-router.put('/:id',tokenValidator,adminParamValidator,(req,res)=>{
+router.put('/:id',middlewares.tokenValidator,middlewares.permissionValidator,(req,res)=>{
     let paramID = req.params.id;
     let user = users.find(e => e.id == paramID);
     const index = users.indexOf(user);
@@ -148,13 +90,13 @@ router.put('/:id',tokenValidator,adminParamValidator,(req,res)=>{
     }
 });
 
-router.delete('/:id',tokenValidator,adminValidator,(req,res)=>{
+router.delete('/:id',middlewares.tokenValidator,middlewares.adminValidator,(req,res)=>{
     let paramID = req.params.id;
     let user = users.find(e => e.id == paramID);
     const index = users.indexOf(user);
     if(user){
         users.splice(index,1);
-        res.status(201).send(`user deleted ${JSON.stringify(user)}`);
+        res.status(200).send(`user deleted ${JSON.stringify(user)}`);
         fs.writeFileSync('users.json',JSON.stringify(users));
         return
     }else{
