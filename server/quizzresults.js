e181f548f3b36400b5fba848d2292c80a775ff4a
marianasrvg <sierravgmr@gmail.com>
2019-12-03 13:45:52 -0600

    api/quizzresults PUT is working with json

--- server/quizzresults.js
+++ server/quizzresults.js
@@ -27,7 +27,54 @@ router.post("/", middlewares.tokenValidator, (req, res) => {
         return;
     }
     delete quizzResults.value.id;
+
+    //Se debería revisar que sea un quizz válido
+
     quizzesResults.push(quizzResults.value);
+    fs.writeFileSync('quizzresults.json',JSON.stringify(quizzesResults));
+    res.status(201).send();
+});
+
+router.put("/:id", middlewares.tokenValidator, (req, res) => {
+    //VERIFICAR USUARIO
+    let id = req.params.id;
+    console.log("Hello "+ id);
+    let quizzIndex = quizzesResults.findIndex(q => q.id == id);
+
+    if(quizzIndex == -1){
+        res.status(404).send("QuizzResult not found");
+    } else {
+        console.log("FOUND AT" + quizzIndex);
+    }
+
+    const schemaQuizz = {
+        quizz: joi.required(),
+        user: joi.required(),
+        nickname: joi.optional(),
+        score: joi.required(),
+        time: joi.optional(),
+        answers: joi.array().items( joi.object({
+            question: joi.required(),
+            time: joi.optional(),
+            correct: joi.required(),
+            answer: joi.required()
+        })),
+        id: joi.required() 
+    }
+
+    let quizz = joi.validate(req.body, schemaQuizz);
+
+    if(quizz.error){
+        res.status(400).send(`Bad request ${quizz.error.details[0].message}`);
+        return;
+    } 
+    quizzesResults[quizzIndex] = quizz.value;
+
+    //BORRAR DESPUES
+    quizzesResults[quizzIndex].id = parseInt(id);
+
+    console.log(quizzesResults);
+
     fs.writeFileSync('quizzresults.json',JSON.stringify(quizzesResults));
     res.status(201).send();
 });
