83f6d98b186818b2f1da0582cf25a8f42966d785
Javier8ap <Javier8apar@gmail.com>
2019-12-03 15:06:45 -0600

    added gets to quizzResults.json

--- server/quizzresults.js
+++ server/quizzresults.js
@@ -1,8 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const middlewares = require('./middlewares');
 
-router.get("/", (req, res) => {
-    res.send("Web Uquizz quizz results");
+const quizzesResults = JSON.parse(fs.readFileSync('quizzresults.json'));
+
+router.get("/",middlewares.tokenValidator, (req, res) => {
+    //quizzes = quizzesResults.find(e => e.user = req.body.id);
+    let quizzes = quizzesResults.filter((e) => {
+        if(e.user == req.body.id){
+            return e;
+        } 
+    });
+
+    res.send(quizzes).status(200);
+    return
+});
+
+router.get('/:pin',middlewares.tokenValidator,(req,res)=>{
+    let pin = req.params.pin;
+    let quizzes = quizzesResults.filter((e)=>{
+        if(e.quizz == pin){
+            return e
+        }
+    });
+
+    res.send(quizzes).status(200);
+    return
 });
 
 module.exports = router;
