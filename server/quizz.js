11025e7bc35f13509eb80596a9b000818359bc73
marianasrvg <sierravgmr@gmail.com>
2019-12-03 12:09:18 -0600

    api/quizz done

--- server/quizz.js
+++ server/quizz.js
@@ -45,7 +45,7 @@ router.get("/", (req, res) => {
             name: item.name,
             description: item.description,
             score: item.score,
-            time: item.time,
+            pin: item.id,
             url: item.url,
         }
         return q;
@@ -54,8 +54,74 @@ router.get("/", (req, res) => {
     res.status(200).send(result);
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:pin", (req, res) => {
+    //VERIFICAR USUARIO
+    let quizz = quizzes.find(q => q.id == req.params.pin);
+    if(quizz == undefined){
+        res.status(404).send("Ese PIN no existe");
+    }
+    res.status(200).send(quizz);
+});
 
+router.put("/:pin", (req, res) => {
+    //VERIFICAR USUARIO
+    let pin = req.params.pin;
+    console.log("Hello"+ pin);
+    let quizzIndex = quizzes.findIndex(q => q.id == pin);
+
+    if(quizzIndex == -1){
+        res.status(404).send("Quizz not found");
+    } else {
+        console.log("FOUND AT" + quizzIndex);
+    }
+
+    const schemaQuizz = {
+        name: joi.required(),
+        description: joi.required(),
+        url: joi.optional(),
+        creator: joi.required(),
+        bestScore: joi.optional(),
+        worstScore: joi.optional(),
+        played: joi.optional(),
+        questions: joi.array().items( joi.object({
+            question: joi.required(),
+            time: joi.optional(),
+            answers: joi.array().items( joi.object({
+                answer: joi.required(),
+                correct: joi.required()
+            }))
+        })),
+        correct: joi.array().items(
+            joi.array().optional()
+        ).optional() 
+    }
+
+    let quizz = joi.validate(req.body, schemaQuizz);
+
+    if(quizz.error){
+        res.status(400).send(`Bad request ${quizz.error.details[0].message}`);
+        return;
+    } 
+    quizzes[quizzIndex] = quizz.value;
+
+    //BORRAR DESPUES
+    quizzes[quizzIndex].id = parseInt(pin);
+
+    fs.writeFileSync('quizz.json',JSON.stringify(quizzes));
+    res.status(201).send();
+});
+
+router.delete("/:pin", (req, res) => {
+    let quizzIndex = quizzes.findIndex(q => q.id == req.params.pin);
+    
+    if(quizzIndex == -1){
+        res.status(404).send("Ese PIN no existe");
+    }
+    
+    quizzes.splice(quizzIndex, 1);
+    fs.writeFileSync('quizz.json',JSON.stringify(quizzes));
+
+    res.status(200).send();
 });
 
 module.exports = router;
